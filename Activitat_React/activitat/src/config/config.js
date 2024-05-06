a139b6fcff70c291d69ad17965062bd4ae064050
jolichh <113618956+jolichh@users.noreply.github.com>
2024-05-06 18:55:36 +0200

    config firebase i install react-rputer-dom

---
+++ Activitat_React/activitat/src/config/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCulzHafKSNgmn0GYvGbFZHKelRXKeJLV4",
+  authDomain: "activitat-react.firebaseapp.com",
+  projectId: "activitat-react",
+  storageBucket: "activitat-react.appspot.com",
+  messagingSenderId: "206705837289",
+  appId: "1:206705837289:web:beb76fef820d4fba481c78",
+  measurementId: "G-BRY6LWZHWS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
