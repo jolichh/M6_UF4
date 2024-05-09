5569a3c77d440e3b4d6a889b69edc70d04d770bc
jolichh <113618956+jolichh@users.noreply.github.com>
2024-05-09 18:59:36 +0200

    loop en movieList

--- Activitat_React/activitat/src/pages/MoviesList.jsx
+++ Activitat_React/activitat/src/pages/MoviesList.jsx
@@ -3,10 +3,25 @@ import { useState } from 'react'
 import MovieCard from '../components/MovieCard'
 
 const MovieList = ({ username }) => {
-  
+  var array_movie = [
+    { title: 'Inception', image: 'inception.jpg', rate: 8.8, direction: 'Christopher Nolan' },
+    { title: 'Interstellar', image: 'interstellar.jpg', rate: 8.6, direction: 'Christopher Nolan' },
+    { title: 'The Dark Knight', image: 'dark_knight.jpg', rate: 9.0, direction: 'Christopher Nolan' },
+  ]
   return (
     <>
         <p>movieslist</p>
+        <ul>
+          {array_movie.map((movie, i) =>(
+            <MovieCard 
+              key={i}
+              title={movie.title}
+              image={movie.image}
+              rate={movie.rate}
+              direction={movie.direction}
+            ></MovieCard>
+          ))}
+        </ul>
     </>
   )
 }
